23588cfe8fe0e975a668f902a4aa409f8c25dd8b
Alumno Plaza Vespucio <capnpncake@gmail.com>
2026-04-22 19:57:51 -0400

    formatting

--- hospitalapp/src/main/java/com/hospital/hospitalapp/model/Atencion.java
+++ hospitalapp/src/main/java/com/hospital/hospitalapp/model/Atencion.java
@@ -1,12 +1,21 @@
 package com.hospital.hospitalapp.model;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table(name = "atencion")
--- hospitalapp/src/main/java/com/hospital/hospitalapp/model/FichaPaciente.java
+++ hospitalapp/src/main/java/com/hospital/hospitalapp/model/FichaPaciente.java
@@ -1,11 +1,15 @@
 package com.hospital.hospitalapp.model;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table(name = "ficha_paciente")
